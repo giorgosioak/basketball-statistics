18b6889705d6b282ec74d4c57de336318fa99db0
George Ioakeimidis <giorgosioak95@gmail.com>
2019-04-18 12:44:52 +0300

    Support players from 0-99 to follow FIBA regulations

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Basketball Statistics #
+# Basketball Statistics [v0.2] #
 Basketball Statistics Calculator made for personal use
 
-![Screenshot of version 0.1](https://i.imgur.com/Nvph90J.png)
+![Screenshot of version 0.2](https://i.imgur.com/drNKOcK.png)
 
 ## Build and run ##
 
@@ -15,20 +15,26 @@ Enter 1 command per line.
 
 Available commands: 
 
-| Command       | What it does      |
-| ------------- | ----------------- |
-| FT x          | Free-Throw Made   |
-| AFT x         | Free-Throw Missed |
-| 2P x          | 2 Point Made      |
-| A2P x         | 2 Point Missed    |
-| M3P x         | 3 Point Made      |
-| A3P x         | 3 Point Missed    |
-| DR x          | Defensive Rebound |
-| OR x          | Offensive Rebound |
-| PF x          | Player Foul       |
-| ON x          | Foul on           |
+| Command           | What it does          |
+| ----------------- | --------------------- |
+| NAME team         | Set the team's name   |
+| SET x MANIKAS D.  | Set player to number x|    |
+| FT x              | Free-Throw Made       |
+| AFT x             | Free-Throw Missed     |
+| 2P x              | 2 Point Made          |
+| A2P x             | 2 Point Missed        |
+| M3P x             | 3 Point Made          |
+| A3P x             | 3 Point Missed        |
+| DR x              | Defensive Rebound     |
+| OR x              | Offensive Rebound     |
+| PF x              | Player Foul           |
+| ON x              | Foul on               |
 
 You have to replace x with the number of the player. For example AFT 12.
+
+## What's new ##
+
+* Dynamic player numbers with a new SET command.
 
 ## Developers ##
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 class Player
 {
   public:
+    int id = 0;     // Player ID number (as shown on uniform)
+    string full_name = "";  // Player Full Name
     int FTM = 0;    // field goals made
     int FTA = 0;    // field goals attemted
     int M2P = 0;    // field goals made
@@ -22,7 +25,8 @@ class Player
 void do_work(string input, int player_id);
 void final_print();
 
-Player player[15] = {};
+Player player[100] = {};
+vector <int> participation;
 Player total = Player();
 int score = 0;
 string team_name = "";
@@ -39,13 +43,19 @@ int main () {
         istringstream iss(input);
         iss >> statistic;
 
-        if (statistic == "show"){
+        if ( statistic == "SHOW" || statistic == "show" ){
             final_print();
-        } else if (statistic == "NAME") {
+        } else if ( statistic == "NAME" || statistic == "name" ) {
             getline(iss, team_name);
-        } else {
+        } else if ( statistic == "SET" || statistic == "set" ) {
             iss >> player_id;
-            do_work(statistic,player_id-1);
+            player[player_id].id = player_id;   // add id to class Player
+            getline(iss, player[player_id].full_name);  // add name to class player
+            participation.push_back(player_id); // add to participated
+        }
+        else {
+            iss >> player_id;
+            do_work(statistic,player_id);
         }
     }
    return 0;
@@ -54,52 +64,52 @@ int main () {
 
 void do_work(string input, int pid){
     
-    if ( input == "FT" ){
+    if ( input == "FT" || input == "ft" ){
         player[pid].FTM++;
         player[pid].FTA++;
         total.FTM++;
         total.FTA++;
         score++;
 
-    } else if ( input == "AFT" ){
+    } else if ( input == "AFT" || input == "aft" ){
         player[pid].FTA++;
         total.FTA++;
 
-    } else if  ( input == "2P" ){
+    } else if  ( input == "2P" || input == "2p" ){
         player[pid].M2P++;
         player[pid].A2P++;
         total.M2P++;
         total.A2P++;
         score+=2;
 
-    } else if  ( input == "A2P" ){
+    } else if  ( input == "A2P" || input == "a2p" ){
         player[pid].A2P++;
         total.A2P++;
 
-    } else if  ( input == "3P" ){
+    } else if  ( input == "3P" || input == "3p" ){
         player[pid].M3P++;
         player[pid].A3P++;
         total.M3P++;
         total.A3P++;
         score+=3;
 
-    } else if  ( input == "A3P" ){
+    } else if  ( input == "A3P" || input == "a3p" ){
         player[pid].A3P++;
         total.A3P++;
 
-    } else if  ( input == "DR" ){
+    } else if  ( input == "DR" || input == "dr" ){
         player[pid].DR++;
         total.DR++;
 
-    } else if  ( input == "OR" ){
+    } else if  ( input == "OR" || input == "or" ){
         player[pid].OR++;
         total.OR++;
 
-    } else if  ( input == "PF" ){
+    } else if  ( input == "PF" || input == "pf" ){
         player[pid].PF++;
         total.PF++;
 
-    } else if  ( input == "ON" ){
+    } else if  ( input == "ON" || input == "on" ){
         player[pid].ON++;
         total.ON++;
         
@@ -111,19 +121,20 @@ void final_print(){
     
     cout << "\tFTM\tFTA\t|\tM2P\tA2P\t|\tM3P\tA3P\t|\tDR\tOR\t|\tPF\tON" << endl;
     
-    for(int i=3;i<15;i++){
-        cout << i+1
-            << "\t" << player[i].FTM
-            << "\t" << player[i].FTA
-            << "\t|\t" << player[i].M2P
-            << "\t" << player[i].A2P
-            << "\t|\t" << player[i].M3P
-            << "\t" << player[i].A3P
-            << "\t|\t" << player[i].DR
-            << "\t" << player[i].OR
-            << "\t|\t" << player[i].PF
-            << "\t" << player[i].ON
-            << "\t" << i+1 << endl;
+    for (int i = 0; i != participation.size(); i++) {
+        int pid = participation[i];
+        cout << pid
+            << "\t" << player[pid].FTM
+            << "\t" << player[pid].FTA
+            << "\t|\t" << player[pid].M2P
+            << "\t" << player[pid].A2P
+            << "\t|\t" << player[pid].M3P
+            << "\t" << player[pid].A3P
+            << "\t|\t" << player[pid].DR
+            << "\t" << player[pid].OR
+            << "\t|\t" << player[pid].PF
+            << "\t" << player[pid].ON
+            << "\t" << pid << "\t" << player[pid].full_name << endl;
     }
 
     cout << "---------------------------------------------------------";
@@ -143,9 +154,5 @@ void final_print(){
     << endl;
 
     cout << "SCORE: " << score << " " << team_name << endl;
-
-}
-
-void set_team_name(string input){
 
 }
